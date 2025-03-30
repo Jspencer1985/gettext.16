674e01bcca872bacadbd629e8931cfac1f0c7ce1
Jeremy <jspencer1985@gmail.com>
2025-03-30 16:46:17 -0700

    rev 8

--- gettext-runtime/intl/fix_macros.h
+++ gettext-runtime/intl/fix_macros.h
@@ -22,11 +22,28 @@
 #define __attribute_malloc__
 #endif
 
-/* Force the use of system string.h instead of custom one */
-#define _STRING_H 1
-#define _STRING_STRING_H 1
+/* Define attribute_hidden as empty */
+#ifndef attribute_hidden
+#define attribute_hidden
+#endif
 
-/* Provide declarations for common string functions */
+/* Define rwlock macros */
+#ifndef gl_rwlock_define
+#define gl_rwlock_define(scope, name) scope pthread_rwlock_t name
+#endif
+
+#ifndef gl_rwlock_wrlock
+#define gl_rwlock_wrlock(lock) pthread_rwlock_wrlock(&(lock))
+#endif
+
+#ifndef gl_rwlock_unlock
+#define gl_rwlock_unlock(lock) pthread_rwlock_unlock(&(lock))
+#endif
+
+/* Include pthread.h for rwlock definitions */
+#include <pthread.h>
+
+/* Include standard string.h */
 #include <string.h>
 
 /* Define strdup if it's not available in the system header */
